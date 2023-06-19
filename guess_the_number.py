80417cafef8f8f9e252c2a5c9e9d99f8133f594d
WiNg4205 <105526030+WiNg4205@users.noreply.github.com>
2023-06-19 12:17:44 +1000

    changed reply for guess the number if guess > 10 or < 0

--- guess_the_number.py
+++ guess_the_number.py
@@ -7,8 +7,12 @@ def start_game():
 
 def run(inpt, num):
     if inpt > num:
+        if inpt > 10:
+            return "Guess between 1-10"
         return "Guess lower"
     elif inpt < num:
+        if inpt < 1:
+            return "Guess between 1-10"
         return "Guess higher"
     else:
         return "You are correct! " + str(inpt) + " was the number."
