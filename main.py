c4377f7373204833e1d19a31742b5a69123449de
Kevin <kkon0188@uni.sydney.edu.au>
2023-06-21 10:40:48 +1000

    notification

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import discord
 import guess_the_number
 import apis
+import threading
+import asyncio
+from datetime import datetime
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -50,5 +53,31 @@ async def on_message(message):
         num = guess_the_number.start_game()
         game = "gtn"
 
+    if msg.startswith("!setNotification"):
+        await send_notification(msg)
 
-client.run("TOKEN")  # replace TOKEN with the actual token
+
+async def send_notification(msg):
+    msg = msg.replace("!setNotification ", "")
+    date_time = msg.split(" ")
+    date_ls = date_time[0].split("-")
+    time_ls = date_time[1].split(":")
+
+    channel = client.get_channel(1109277709806338090)
+
+    if (len(date_ls) != 3) or (len(time_ls) != 3):
+        await channel.send("Wrong format")
+        return
+    
+    try:
+        scheduled_time = datetime(int(date_ls[2]), int(date_ls[1]), int(date_ls[0]), int(time_ls[0]), int(time_ls[1]), int(time_ls[2]))
+        while datetime.now() < scheduled_time:
+            await asyncio.sleep(1)
+    except ValueError:
+        await channel.send("Invalid time")
+        return
+        
+    await channel.send("notification")
+
+
+client.run("MTExMTgwMDkxNjU4OTQzMjkwNg.GPUKV-.cCcYBeFtoV94kOeLW-BQwPc5eoGchMTX5h4PKw")  # replace TOKEN with the actual token
